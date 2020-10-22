0d0e4f3ec1baca9549e5769f181d0c848ad5f914
mstsgh <steinberg.mirko@web.de>
2020-10-22 11:15:50 +0200

    New

--- Reiskörner_Schachbrett.py
+++
@@ -1,9 +0,0 @@
-summe = 0
-for feld in range(64):
-    reiskorn = 2**feld
-    summe = summe + reiskorn
-    print("Feld {}. = {:>30,} Reiskörner und damit insgesamt {:>30,} Reiskörner" \
-        .format(feld+1,reiskorn,summe))
-gewicht = summe * 0.02 / 1000 / 1000
-print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten ")
-print("Reiskörner {:18,.0f} Tonnen".format(gewicht)) 
