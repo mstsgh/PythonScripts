89857863c7ebaccc257bfe6a727eb20c04418110
mstsgh <steinberg.mirko@web.de>
2020-10-22 11:56:53 +0200

    Monty Python's SPAM Song in Python

---
+++ spam.py
@@ -0,0 +1,14 @@
+from random import shuffle
+
+superlatives = "amazing gorgeous blazing stunning tremendous greatest best fantastic \
+               phenomenal delightful ambitious outstanding incredible spectacular \
+               super cool magical revolutionary beautiful jaw-dropping lovely".upper().split()
+shuffle(superlatives)
+
+for strophe in range(5):
+    for n in range(2):
+        for i in range(4):
+            print("Spam", end=' ')
+        print()
+    print(superlatives.pop() + " Spam, " + superlatives.pop() + " Spam")
+    print()
